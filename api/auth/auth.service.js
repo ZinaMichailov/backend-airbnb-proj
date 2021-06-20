59daa8a478eb9c4defb65f203610de77d8ef63c7
zina <zinam84@gmail.com>
2021-06-20 12:11:54 +0300

    auth

--- api/auth/auth.service.js
+++ api/auth/auth.service.js
@@ -3,10 +3,10 @@ const userService = require('../user/user.service')
 const logger = require('../../services/logger.service')
 
 
-async function login(username, password) {
-    logger.debug(`auth.service - login with username: ${username}`)
+async function login(email, password) {
+    logger.debug(`auth.service - login with username: ${email}`)
 
-    const user = await userService.getByUsername(username)
+    const user = await userService.getByUsername(email)
     if (!user) return Promise.reject('Invalid username or password')
     const match = await bcrypt.compare(password, user.password)
     if (!match) return Promise.reject('Invalid username or password')
@@ -15,14 +15,14 @@ async function login(username, password) {
     return user
 }
 
-async function signup(username, password, fullname, imgUrl) {
+async function signup(email, password, fullname, imgUrl) {
     const saltRounds = 10
 
-    logger.debug(`auth.service - signup with username: ${username}, fullname: ${fullname}`)
-    if (!username || !password || !fullname) return Promise.reject('fullname, username and password are required!')
+    logger.debug(`auth.service - signup with email: ${email}, fullname: ${fullname}`)
+    if (!email || !password || !fullname) return Promise.reject('fullname, email and password are required!')
 
     const hash = await bcrypt.hash(password, saltRounds)
-    return userService.add({ username, password: hash, fullname, imgUrl })
+    return userService.add({ email, password: hash, fullname, imgUrl })
 }
 
 module.exports = {
